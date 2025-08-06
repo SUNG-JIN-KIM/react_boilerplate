9042f5b0a59259992db3f80d3b59445198f6bd9d
user <ksjcloud98@gmail.com>
2025-08-06 11:46:10 +0900

    R commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,7 @@
-import { useState } from "react";
-import A from "./components/A";
-import B from "./components/B";
-
 const App = () => {
-  const [count, setCount] = useState(0);
-
-  const increase = () => setCount(count + 1);
-
   return (
     <div className="bg-red-100 min-h-screen flex justify-center items-center">
-      <A count={count} increase={increase} />
-      <B setCount={setCount} />
+      App
     </div>
   );
 };
